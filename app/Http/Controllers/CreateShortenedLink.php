ea04ecf5d493796c0d9f5f41388d33f8315586f0
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2021-12-27 19:25:26 +0100

    Return back uploading to database

--- app/Http/Controllers/CreateShortenedLink.php
+++ app/Http/Controllers/CreateShortenedLink.php
@@ -19,12 +19,12 @@ class CreateShortenedLink extends Controller
             // and uploading it to database
             $s = hash('crc32', $url);
 
-            // check if duplicate
-            // $result = DB::select('select original_link from shortened where original_link = :original_link', ['original_link' => $url]);
-            // if (sizeof($result) == 0)
-            //     DB::insert('insert into shortened (original_link, shortened_link, created_at, last_used) values (?, ?, ?, ?)', [$url, $s, now(), now()]);
-            // else
-            //     DB::update('update shortened set last_used = ? where original_link = ?', [now(), $url]); // if duplicate just update last_used
+            //check if duplicate
+            $result = DB::select('select original_link from shortened where original_link = :original_link', ['original_link' => $url]);
+            if (sizeof($result) == 0)
+                DB::insert('insert into shortened (original_link, shortened_link, created_at, last_used) values (?, ?, ?, ?)', [$url, $s, now(), now()]);
+            else
+                DB::update('update shortened set last_used = ? where original_link = ?', [now(), $url]); // if duplicate just update last_used
 
             return redirect('/')->with('status', 'Shortened successfully')->with('qr', $this->createQR($url))->with('url', $_SERVER['HTTP_HOST'] . '/' . $s);
         } else
